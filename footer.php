b52018be8622254207d9e76e4780d7053d3e1012
WahyuBagasDwiPrasetyo <111216156+WahyuBagasDwiPrasetyo@users.noreply.github.com>
2023-03-19 22:12:24 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p> Design by <a target="_blank" href="https://github.com/">Wahyu Bagas Dwi Prasetyo</a> </p>
+</footer>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,7 @@
+<div class="menu">
+    <a href="index.php">Jaminan Hari Tua | </a>
+    <a href="hitung-jaminan-kecelakaan.php">Jaminan Kecelakaan Kerja | </a>
+    <a href="hitung-jaminan-kematian.php">Jaminan Kematian | </a>
+    <a href="hitung-jaminan-pensiun.php">Jaminan Pensiun |</a>
+    <a href="FB.php">Feedback</a>
+</div>
